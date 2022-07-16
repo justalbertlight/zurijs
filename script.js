f22dd35d2a9769f563f6a55677555cbe626e5862
justalbertlight <104940651+justalbertlight@users.noreply.github.com>
2022-07-16 05:13:20 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = 'Albert Ogunyemi';
+let height = '6ft'
+let country = 'Nigeria'
+
+console.log('Hello there!, My name is ' + name);
+console.log('I am ' + height + ' tall');
+console.log('And I am from ' + country);
